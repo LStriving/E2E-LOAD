b1345e59c944e11387792dc7b892a68a595cebc7
Lstriving <2625847986@qq.com>
2024-02-15 17:50:14 +0800

    add eqa loss

--- src/models/losses.py
+++ src/models/losses.py
@@ -123,6 +123,131 @@ class MultipCrossEntropyLoss(nn.Module):
             else:
                 return output
 
+class EQLv2Loss(nn.Module):
+
+    def __init__(self, gamma=12, mu=8, alpha=4.0, reduction='mean', ignore_index=None,
+                 num_classes=3806):
+        super(EQLv2Loss, self).__init__()
+        self.num_classes = num_classes - 1
+
+        self.gamma = gamma
+        self.mu = mu
+        self.alpha = alpha
+        self.ignore_index = ignore_index
+
+        self._pos_grad = None
+        self._neg_grad = None
+        self.pos_neg = None
+
+        def _func(x, gamma, mu):
+            return 1 / (1 + torch.exp(-gamma * (x - mu)))
+        self.map_func = partial(_func, gamma=self.gamma, mu=self.mu)
+
+    def forward(self, input, target):
+        self.n_i, self.n_c = input.size()
+        # print(self.n_i, self.n_c)
+        pos_w, neg_w = self.get_weight(input)
+
+        weight = pos_w * target + neg_w * (1 - target)
+
+        cls_loss = F.binary_cross_entropy_with_logits(input, target, reduction='none')
+        cls_loss = torch.sum(cls_loss * weight) / self.n_i
+
+        self.collect_grad(input.detach(), target.detach(), weight.detach())
+
+        return cls_loss
+
+    def collect_grad(self, cls_score, target, weight):
+        prob = torch.sigmoid(cls_score)
+        grad = target * (prob - 1) + (1 - target) * prob # prob - target
+        grad = torch.abs(grad)
+
+        # do not collect grad for objectiveness branch [:-1]
+        if self.ignore_index is not None:
+            grad[:, self.ignore_index] = 0
+        pos_grad = torch.sum(grad * target * weight, dim=0)[:-1]
+        neg_grad = torch.sum(grad * (1 - target) * weight, dim=0)[:-1]
+
+        # dist.all_reduce(pos_grad)
+        # dist.all_reduce(neg_grad)
+
+        self._pos_grad += pos_grad
+        self._neg_grad += neg_grad
+        self.pos_neg = self._pos_grad / (self._neg_grad + 1e-10)
+
+    def get_weight(self, cls_score):
+        if self._pos_grad is None:
+            self._pos_grad = cls_score.new_zeros(self.num_classes)
+            self._neg_grad = cls_score.new_zeros(self.num_classes)
+            neg_w = cls_score.new_zeros((self.n_i, self.n_c))
+            pos_w = cls_score.new_zeros((self.n_i, self.n_c))
+        else:
+            neg_w = torch.cat([self.map_func(self.pos_neg),cls_score.new_ones(1)])
+            pos_w = 1 + self.alpha * (1 - neg_w)
+            neg_w = neg_w.view(1, -1).expand(self.n_i, self.n_c)
+            pos_w = pos_w.view(1, -1).expand(self.n_i, self.n_c)
+        return pos_w, neg_w
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0, reduction='mean', ignore_index=-100):
+        super().__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+    
+    def forward(self, input, target):
+        return sigmoid_focal_loss(input, target, self.alpha, self.gamma, self.reduction)
+    
+
+@torch.jit.script
+def sigmoid_focal_loss(
+    inputs: torch.Tensor,
+    targets: torch.Tensor,
+    alpha: float = 0.25,
+    gamma: float = 2.0,
+    reduction: str = "none",
+) -> torch.Tensor:
+    """
+    Loss used in RetinaNet for dense detection: https://arxiv.org/abs/1708.02002.
+    Taken from
+    https://github.com/facebookresearch/fvcore/blob/master/fvcore/nn/focal_loss.py
+    # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+    Args:
+        inputs: A float tensor of arbitrary shape.
+                The predictions for each example.
+        targets: A float tensor with the same shape as inputs. Stores the binary
+                 classification label for each element in inputs
+                (0 for the negative class and 1 for the positive class).
+        alpha: (optional) Weighting factor in range (0,1) to balance
+                positive vs negative examples. Default = 0.25.
+        gamma: Exponent of the modulating factor (1 - p_t) to
+               balance easy vs hard examples.
+        reduction: 'none' | 'mean' | 'sum'
+                 'none': No reduction will be applied to the output.
+                 'mean': The output will be averaged.
+                 'sum': The output will be summed.
+    Returns:
+        Loss tensor with the reduction option applied.
+    """
+    inputs = inputs.float()
+    targets = targets.float()
+    p = torch.sigmoid(inputs)
+    ce_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
+    p_t = p * targets + (1 - p) * (1 - targets)
+    loss = ce_loss * ((1 - p_t) ** gamma)
+
+    if alpha >= 0:
+        alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
+        loss = alpha_t * loss
+
+    if reduction == "mean":
+        loss = loss.mean()
+    elif reduction == "sum":
+        loss = loss.sum()
+
+    return loss
+
 
 _LOSSES = {
     "cross_entropy": nn.CrossEntropyLoss,
