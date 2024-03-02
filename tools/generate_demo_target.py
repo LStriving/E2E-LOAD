1f14b01af27f2abbfac1bee7539e11efc5228680
LStriving <2625847986@qq.com>
2024-03-02 21:39:14 +0800

    add support for single video

--- tools/generate_demo_target.py
+++ tools/generate_demo_target.py
@@ -2,18 +2,20 @@ import os
 import numpy as np
 from tqdm import tqdm
 
-def generate(videos_frames_root, output_dir):
+def generate(videos_frames_root, output_dir, video_name=None, resume=True):
     """
     Generate the target file for the demo.
     """
     if not os.path.exists(output_dir):
         print("Creating directory: {}".format(output_dir))
         os.makedirs(output_dir)
-    
-    for video in tqdm(os.listdir(videos_frames_root)):
+    if video_name is not None:
+        to_extract = [video_name]
+    for video in tqdm(to_extract if video_name is not None else os.listdir(videos_frames_root)):
         video_path = os.path.join(videos_frames_root, video)
         target_path = os.path.join(output_dir, video + ".npy")
-        
+        if resume and os.path.exists(target_path):
+            continue
         frame_length = len(os.listdir(video_path))
         
         num_chunks = int(frame_length // 6)
